fcbbdd97960f68f0ab3c8612b89b1950bf58a353
hadfieldcj@gmail.com <hadfieldcj@gmail.com>
2018-04-29 14:20:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var name = "Joe"
 var height = "74"
 
+if(name === "Susan") {
+  console.log(name)
+} else {
+  console.log("Expected name to be Susan")
+}
 
 // Don't worry about this
 module.exports = { name, height
